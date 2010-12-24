9f1361e2a2a23ee2e6794ddab4ed946790ad6c0a
Nathanael <jerede59@hotmail.fr>
2010-12-24 19:14:34 +0100

    Amellioration de la classe ExtendedLook. Joyeux Noel
    Signed-off-by: Nathanael <jerede59@hotmail.fr>

--- trunk/DofusProtocol/Classes/Custom/ExtendedLook.cs
+++ trunk/DofusProtocol/Classes/Custom/ExtendedLook.cs
@@ -9,18 +9,13 @@ namespace Stump.DofusProtocol.Classes.Custom
 {
     public class ExtendedLook
     {
-
+        
         #region Fields
 
-        private uint m_bonesId;
         public uint BonesId
         {
-            get { return m_bonesId; }
-            set
-            {
-                m_bonesId = value;
-                m_updated = true;
-            }
+            get { return m_entityLook.bonesId; }
+            set { m_entityLook.bonesId = value; }
         }
 
         private List<uint> m_characterSkins;
@@ -30,7 +25,7 @@ namespace Stump.DofusProtocol.Classes.Custom
             set
             {
                 m_characterSkins = value;
-                m_updated = true;
+                UpdateEntityLookSkins();
             }
         }
 
@@ -41,93 +36,50 @@ namespace Stump.DofusProtocol.Classes.Custom
             set
             {
                 m_itemSkins = value;
-                m_updated = true;
+                UpdateEntityLookSkins();
             }
         }
 
-        private List<int> m_colors;
         public List<int> Colors
         {
-            get { return m_colors; }
-            set
-            {
-                m_colors = value;
-                m_updated = true;
-            }
+            get { return m_entityLook.indexedColors; }
+            set { m_entityLook.indexedColors = value; }     
+           
         }
 
-        private List<int> m_scales;
         public List<int> Scales
         {
-            get { return m_scales; }
-            set
-            {
-                m_scales = value;
-                m_updated = true;
-            }
+            get { return m_entityLook.scales; }
+            set { m_entityLook.scales = value; }       
         }
 
-        private List<SubEntity> m_subentities;
         public List<SubEntity> Subentities
         {
-            get { return m_subentities; }
-            set
-            {
-                m_subentities = value;
-                m_updated = true;
-            }
+            get { return m_entityLook.subentities; }
+            set { m_entityLook.subentities = value; }
         }
-
-        private bool m_updated = true;
 
         private EntityLook m_entityLook;
         public EntityLook EntityLook
         {
-            get
-            {
-                if (m_updated)
-                    UpdateEntityLook();
-                return m_entityLook;
-            }
+            get{ return m_entityLook;}
         }
-
+            
         #endregion
 
-        #region Ctors
-
         public ExtendedLook()
-        {
-            m_bonesId = 0;
-            m_characterSkins = new List<uint>(2);
+        {   
+            m_entityLook = new EntityLook();
+            m_characterSkins = new List<uint>();
             m_itemSkins = new Dictionary<CharacterInventoryPositionEnum, uint>();
-            m_colors = new List<int>(5);
-            m_scales = new List<int>(2);
-            m_subentities = new List<SubEntity>();
         }
 
         public ExtendedLook(EntityLook baseLook)
         {
-            m_bonesId = baseLook.bonesId;
+            m_entityLook = baseLook;
             m_characterSkins = baseLook.skins;
             m_itemSkins = new Dictionary<CharacterInventoryPositionEnum, uint>();
-            m_colors = baseLook.indexedColors;
-            m_scales = baseLook.scales;
-            m_subentities = baseLook.subentities;
         }
-
-        public ExtendedLook(uint bonesId, List<uint> characterSkins, Dictionary<CharacterInventoryPositionEnum,uint> itemSkins, List<int> colors, List<int> scales, List<SubEntity> subEntities)
-        {
-            m_bonesId = bonesId;
-            m_characterSkins = characterSkins;
-            m_itemSkins = itemSkins;
-            m_colors = colors;
-            m_scales = scales;
-            m_subentities = subEntities;
-        }
-
-        #endregion
-
-        #region Update Look
 
         private void SetItemLook(uint look, CharacterInventoryPositionEnum position)
         {
@@ -135,21 +87,13 @@ namespace Stump.DofusProtocol.Classes.Custom
                 m_itemSkins[position] = look;
             else
                 m_itemSkins.Add(position, look);
-            m_updated = true;
+            UpdateEntityLookSkins();
         }
 
-        private void UpdateEntityLook()
+        private void UpdateEntityLookSkins()
         {
-            m_entityLook = new EntityLook(m_bonesId,m_characterSkins.Concat(m_itemSkins.Values).ToList() , m_colors, m_scales, m_subentities);
-            m_updated = false;
-            OnUpdateEntityLook();
+            m_entityLook.skins = m_characterSkins.Concat(m_itemSkins.Values).ToList();
         }
-
-        private void OnUpdateEntityLook()
-        {
-        }
-
-        #endregion
 
     }
 }
