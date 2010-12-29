780a22ba3d299ba1ad84c0f26d42bd14460e4087
bouh2 <bouh22@hotmail.fr>
2010-12-29 22:09:17 +0100

    Ajout de DataLoader pour manier facilement les D2O & co
    Signed-off-by: bouh2 <bouh22@hotmail.fr>

---
+++ trunk/Tools/Stump.Tools.DataLoader/D2OAdapater.cs
@@ -0,0 +1,165 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Stump.DofusProtocol.D2oClasses;
+using Stump.Server.BaseServer.Data.D2oTool;
+
+
+namespace Stump.Tools.DataLoader
+{
+    public class D2OAdapater : IFileAdapter
+    {
+        private static I18nFile m_lastI18NFile;
+
+        private static readonly Dictionary<string,Type> m_typeByFileName = new Dictionary<string, Type>();
+
+        static D2OAdapater()
+        {
+            foreach (var type in typeof(AttributeAssociatedFile).Assembly.GetTypes())
+            {
+                var attribute = (AttributeAssociatedFile) type.GetCustomAttributes(typeof(AttributeAssociatedFile), false).FirstOrDefault();
+
+                if (attribute != null)
+                {
+                    m_typeByFileName.Add(attribute.FilesName.FirstOrDefault(), type);
+                }
+            }
+        }
+        
+        public D2OAdapater()
+        {
+            MenuItem = new ToolStripMenuItem("D2O");
+            MenuItem.DropDownItems.Add("Convert Name's ID by Text...", null, AttachToI18N);
+        }
+
+        public D2OAdapater(string file)
+            : this()
+        {
+            FileName = file;
+        }
+
+        public string FileName
+        {
+            get;
+            private set;
+        }
+
+        public string ExtensionSupport
+        {
+            get { return "d2o"; }
+        }
+        private FormD2O m_form;
+        public Form Form
+        {
+            get { return m_form; }
+        }
+
+        public ToolStripMenuItem MenuItem
+        {
+            get;
+            private set;
+        }
+
+        public void Open()
+        {
+            string file = Path.GetFileNameWithoutExtension(FileName);
+
+            if (!m_typeByFileName.ContainsKey(file))
+                throw new ArgumentException("'{0}' is not a valid D2O file", FileName);
+
+            m_form = new FormD2O {Text = Path.GetFileName(file), Adapter = this};
+            FillDataView();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void FillDataView()
+        {
+            var datafile = new D2oFile(FileName);
+
+            Dictionary<int, D2oClassDefinition> classes = datafile.GetClasses();
+            string[] columns = classes.Values.First().Fields.Select(entry => entry.Key).ToArray();
+            m_form.DefineColumns(columns);
+
+            var copy = new ConcurrentStack<object>();
+            foreach (var @class in classes)
+            {
+                try
+                {
+                    object data = datafile.ReadObject(@class.Key);
+
+                    if (data != null)
+                    {
+                        var fields =
+                            data.GetType().GetFields(BindingFlags.Public | BindingFlags.GetField | BindingFlags.Instance).
+                            ToDictionary(entry=> entry.Name, entry => entry.GetValue(data));
+
+                        var values = new object[columns.Length];
+                        for (int i = 0; i < columns.Length; i++)
+                        {
+                            values[i] = fields[columns[i]];
+                        }
+
+                        m_form.AddRow(values);
+                    }
+                    else
+                    {
+                        m_form.AddRow("(null)");
+                    }
+                }
+                catch
+                {
+                    m_form.AddRow(string.Format("Error thrown when parsing (?) <id:{0}>", @class.Key));
+                }
+            }
+        }
+
+        private void AttachToI18N(object sender, EventArgs eventArgs)
+        {
+            var columns = (from DataGridViewColumn entry in m_form.m_dataView.Columns
+                           select entry.HeaderText).ToArray();
+
+            var dialogSelect = new FormSelect(columns, columns.Where(entry => entry.ToLower().Contains("nameid")))
+                {Text = @"Select columns to convert..."};
+
+            if (dialogSelect.ShowDialog(Form) == DialogResult.OK)
+            {
+                var dialog = new OpenFileDialog
+                    {
+                        Title = @"Select the d2i file used to found the text by the name's id...",
+                        CheckFileExists = true,
+                        CheckPathExists = true,
+                        Filter = @"d2i files (*.d2i)|*.d2i",
+                        Multiselect = false
+                    };
+
+                if (m_lastI18NFile != null || dialog.ShowDialog(Form) == DialogResult.OK)
+                {
+                    if (m_lastI18NFile == null)
+                        m_lastI18NFile = new I18nFile(dialog.FileName);
+
+                    for (int i = 0; i < m_form.m_dataView.Rows.Count; i++)
+                    {
+                        foreach (var column in dialogSelect.SelectedStrings)
+                        {
+                            if (m_form.m_dataView.Rows[i].Cells[column].Value is int)
+                                m_form.m_dataView.Rows[i].Cells[column].Value =
+                                    m_lastI18NFile.ReadText((int)m_form.m_dataView.Rows[i].Cells[column].Value);
+                            else if (m_form.m_dataView.Rows[i].Cells[column].Value is uint)
+                                m_form.m_dataView.Rows[i].Cells[column].Value =
+                                    m_lastI18NFile.ReadText((int)( (uint)m_form.m_dataView.Rows[i].Cells[column].Value ));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ trunk/Tools/Stump.Tools.DataLoader/FormMain.cs
@@ -0,0 +1,133 @@
+﻿// /*************************************************************************
+//  *
+//  *  Copyright (C) 2010 - 2011 Stump Team
+//  *
+//  *  This program is free software: you can redistribute it and/or modify
+//  *  it under the terms of the GNU General Public License as published by
+//  *  the Free Software Foundation, either version 3 of the License, or
+//  *  (at your option) any later version.
+//  *
+//  *  This program is distributed in the hope that it will be useful,
+//  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  *  GNU General Public License for more details.
+//  *
+//  *  You should have received a copy of the GNU General Public License
+//  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//  *
+//  *************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Stump.Tools.DataLoader
+{
+    public partial class FormMain : Form
+    {
+        private static readonly Dictionary<string, Type> m_adapters = new Dictionary<string, Type>();
+
+        static FormMain()
+        {
+            foreach (
+                Type type in
+                    typeof (IFileAdapter).Assembly.GetTypes().Where(
+                        entry => entry.GetInterfaces().Contains(typeof (IFileAdapter))))
+            {
+                var adapter = (IFileAdapter) Activator.CreateInstance(type);
+
+                m_adapters.Add(adapter.ExtensionSupport, type);
+            }
+        }
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            string filter = m_adapters.Aggregate("",
+                                                 (current, adapter) =>
+                                                 current + string.Format("{0} files (*.{1})|*.{1}|", adapter.Key, adapter.Key));
+
+            filter = filter.Remove(filter.Length - 1);
+
+            var dialog = new OpenFileDialog
+                {
+                    CheckFileExists = true,
+                    CheckPathExists = true,
+                    Filter = filter,
+                    Multiselect = false
+                };
+
+            if (dialog.ShowDialog(this) == DialogResult.OK)
+            {
+                OpenFile(dialog.FileName);
+            }
+        }
+
+        private void OpenFile(string fileName)
+        {
+            try
+            {
+                string ext = Path.GetExtension(fileName).Remove(0, 1).ToLower();
+
+                if (!m_adapters.ContainsKey(ext))
+                    throw new Exception(ext + " is not a valide extension");
+
+                var adapter = (IFileAdapter)Activator.CreateInstance(m_adapters[ext], fileName);
+
+                adapter.Open();
+
+                adapter.Form.MdiParent = this;
+                adapter.Form.Show();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(this, "Cannot open this file : " + e.Message, "Cannot open file", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void FormMainMdiChildActivate(object sender, EventArgs e)
+        {
+            if (menuStrip1.Items[1].Tag is IFileAdapter)
+                menuStrip1.Items.RemoveAt(1);
+
+            if (ActiveMdiChild != null)
+            {
+                menuStrip1.Items.Insert(1, ((FormAdapter) ActiveMdiChild).Adapter.MenuItem);
+                menuStrip1.Items[1].Tag = ((FormAdapter) ActiveMdiChild).Adapter;
+            }
+        }
+
+        private void ExitToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void FormMainDragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false))
+            {
+                e.Effect = DragDropEffects.Move;
+            }
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void FormMainDragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false))
+            {
+                var files = (object[])e.Data.GetData(DataFormats.FileDrop, false);
+
+                foreach (var file in files)
+                {
+                    OpenFile(file.ToString());
+                }
+            }
+        }
+    }
+}
