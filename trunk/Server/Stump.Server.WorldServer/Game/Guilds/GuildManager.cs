39f23b0385ec33c5243a612dd749e0b36aeb81d0
SpheX <arnaud.cout@gmail.com>
2013-09-05 11:02:15 +0200

    For test guilds

---
+++ trunk/Server/Stump.Server.WorldServer/Game/Guilds/GuildManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Stump.Core.Reflection;
+using Stump.Server.BaseServer.Database;
+using Stump.Server.BaseServer.IPC.Objects;
+using Stump.Server.WorldServer.Core.IPC;
+using Stump.Server.WorldServer.Core.Network;
+using Stump.Server.WorldServer.Database;
+using Stump.Server.WorldServer.Database.Guilds;
+
+namespace Stump.Server.WorldServer.Game.Guilds
+{
+    public class GuildManager : DataManager<GuildManager>
+    {
+        /// <summary>
+        /// Returns null if not found
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public GuildRecord FindById(int id)
+        {
+            return Database.FirstOrDefault<GuildRecord>(string.Format(GuildRelator.FetchById, id));
+        }
+    }
+}
