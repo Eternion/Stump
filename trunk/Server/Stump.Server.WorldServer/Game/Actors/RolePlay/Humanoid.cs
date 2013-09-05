6bc957fb5e3e6c711fde1506de7f9ee26239ae75
Orochi <orochi@arkalys.com>
2013-09-05 15:57:44 +0200

    Add SendGuildInformationsGeneralMessage and add HumanOptionGuild

--- trunk/Server/Stump.Server.WorldServer/Game/Actors/RolePlay/Humanoid.cs
+++ trunk/Server/Stump.Server.WorldServer/Game/Actors/RolePlay/Humanoid.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using Stump.Core.Extensions;
 using Stump.DofusProtocol.Enums;
 using Stump.DofusProtocol.Types;
+using Stump.Server.WorldServer.Game.Actors.RolePlay.Characters;
+using Stump.Server.WorldServer.Game.Guilds;
 
 namespace Stump.Server.WorldServer.Game.Actors.RolePlay
 {
@@ -36,13 +38,26 @@ namespace Stump.Server.WorldServer.Game.Actors.RolePlay
 
         #region HumanInformations
 
-        public virtual HumanInformations GetHumanInformations()
+        public virtual HumanInformations GetHumanInformations(Character character)
         {
             //IEnumerable<HumanOption> hOptions = new HumanOption[] { new HumanOptionEmote(22, DateTime.Now.GetUnixTimeStamp()), new HumanOptionGuild(new GuildInformations(61001, "Staff", new GuildEmblem(104, 9864735, 15, 16777215))) };
+            var hOptions = Enumerable.Empty<HumanOption>();
+
+            var guildId = character.GuildId;
+
+            if (guildId != 0)
+            {
+
+                var guildInfo = GuildManager.Instance.FindById(guildId);
+                hOptions = new HumanOption[]
+                    {
+                        new HumanOptionGuild(new GuildInformations(guildId, guildInfo.Name, guildInfo.GuildEmblem))
+                    };
+            }
 
             return new HumanInformations(new ActorRestrictionsInformations(),
                 Sex == SexTypeEnum.SEX_FEMALE,
-                Enumerable.Empty<HumanOption>()); // todo
+                hOptions); // todo
         }
 
         #endregion 
