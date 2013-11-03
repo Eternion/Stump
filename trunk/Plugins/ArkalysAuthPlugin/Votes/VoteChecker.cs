e12e1a86581063acf8c485e256277dc7f8ed7e03
bouh2 <bouh22@hotmail.fr>
2013-11-03 19:46:25 +0100

    Add vote checker

---
+++ trunk/Plugins/ArkalysAuthPlugin/Votes/VoteChecker.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using Stump.Core.Reflection;
+using Stump.Server.AuthServer;
+using Stump.Server.AuthServer.Database;
+using Stump.Server.AuthServer.Managers;
+using Stump.Server.BaseServer.Initialization;
+
+namespace ArkalysAuthPlugin.Votes
+{
+    public class VoteChecker : Singleton<VoteChecker>
+    {
+        [Initialization(InitializationPass.Last)]
+        public void Initialize()
+        {
+            AuthServer.Instance.IOTaskPool.CallPeriodically(10000, CheckVotes);
+        }
+
+        private void CheckVotes()
+        {
+
+            // select all votes between 1 week and 3 hours ago, limited by 1 vote by account (the one with the last vote)
+            var votes =
+                AuthServer.Instance.DBAccessor.Database.Fetch<VoteRecord>(
+                    string.Format("SELECT * FROM Votes INNER JOIN (SELECT UserID, MAX(Date) as MaxDate FROM Votes GROUP BY UserID)" +
+                    " grp ON Votes.UserID = grp.UserID AND Votes.Date = grp.MaxDate WHERE grp.MaxDate BETWEEN '{0}' AND '{1}'",
+                    (DateTime.Now - TimeSpan.FromDays(7)).ToString("yyyy-MM-dd  hh:mm:ss"), (DateTime.Now - TimeSpan.FromHours(3)).ToString("yyyy-MM-dd hh:mm:ss")));
+
+            if (votes.Count == 0)
+                return;
+
+            var accounts = AuthServer.Instance.DBAccessor.Database.Query<Account>(
+                string.Format("SELECT * FROM accounts WHERE Id IN ({0})",
+                    string.Join(",", votes.Select(x => x.AccountId)))).ToDictionary(x => x.Id);
+
+            var groupedVotes = from vote in votes
+                where accounts.ContainsKey(vote.AccountId)
+                let acc = accounts[vote.AccountId]
+                where acc.LastConnectionWorld != null
+                group vote by acc.LastConnectionWorld.Value;
+
+            foreach (var group in groupedVotes)
+            {
+                var world = WorldServerManager.Instance.GetServerById(group.Key);
+
+                if (world.IPCClient == null)
+                    continue;
+
+                world.IPCClient.Send(new VoteNotificationMessage(group.Select(x => x.AccountId).ToArray()));
+            }
+        }
+    }
+}
---
+++ trunk/Plugins/ArkalysPlugin/Votes/VoteNotificationHandler.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Linq;
+using Stump.Server.BaseServer.Initialization;
+using Stump.Server.BaseServer.IPC;
+using Stump.Server.WorldServer;
+using Stump.Server.WorldServer.Core.IPC;
+using Stump.Server.WorldServer.Game;
+
+namespace ArkalysPlugin.Votes
+{
+    public static class VoteNotificationHandler
+    {
+        [Initialization(InitializationPass.Last, Silent=true)]
+        public static void Initialize()
+        {
+            IPCAccessor.Instance.AddMessageHandler(typeof (VoteNotificationMessage), HandleMessage);
+        }
+
+        private static void HandleMessage(IPCMessage message)
+        {
+            var msg = (VoteNotificationMessage) message;
+
+            foreach (var character in World.Instance.GetCharacters(x => msg.AccountsToNotify.Contains(x.Account.Id)))
+            {
+                character.DisplayNotification(
+                    "Plus de 3H se sont écoulées depuis votre dernier vote, vous pouvez à nouveau voter pour gagner des jetons depuis le site arkalys.com ou l'uplauncher");
+            }
+        }
+    }
+}
