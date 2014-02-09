f53fcaa9fd9f269d46a572b7d8872ec3a46f11cc
Orochi <orochi@arkalys.com>
2014-02-09 01:43:01 +0100

    Apply a fix for jail bypass if create a new character.

--- trunk/Server/Stump.Server.WorldServer/Game/Maps/Map.cs
+++ trunk/Server/Stump.Server.WorldServer/Game/Maps/Map.cs
@@ -1107,6 +1107,12 @@ namespace Stump.Server.WorldServer.Game.Maps
                 monsterGroup.MoveTimer = Area.CallDelayed(new Random().Next(MonsterGroup.MinMoveInterval, MonsterGroup.MaxMoveInterval + 1) * 1000,
                     () => MoveRandomlyMonsterGroup(monsterGroup));
             }
+
+            if (character == null)
+                return;
+
+            if (character.Account.IsJailed)
+                character.TeleportToJail();
         }
 
         private void SendActorsActions(Character character)
