7da821f3d3b313ed97e8852bd22822bdd1fb056d
Orochi <orochi@arkalys.com>
2014-02-12 14:10:57 +0100

    Fix Dialog TaxCollector

--- trunk/Server/Stump.Server.WorldServer/Game/Actors/RolePlay/TaxCollectors/TaxCollectorNpc.cs
+++ trunk/Server/Stump.Server.WorldServer/Game/Actors/RolePlay/TaxCollectors/TaxCollectorNpc.cs
@@ -6,6 +6,7 @@ using Stump.Core.Threading;
 using Stump.DofusProtocol.Enums;
 using Stump.DofusProtocol.Messages;
 using Stump.DofusProtocol.Types;
+using Stump.Server.WorldServer.Database.I18n;
 using Stump.Server.WorldServer.Database.World;
 using Stump.Server.WorldServer.Game.Actors.Look;
 using Stump.Server.WorldServer.Game.Dialogs.TaxCollector;
