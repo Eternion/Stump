92fef535bdb9add8e335adf7339ea57c4a1275c2
Orochi <orochi@arkalys.com>
2014-03-03 14:59:05 +0100

    Fix guild kick

--- trunk/Server/Stump.Server.WorldServer/Game/Guilds/Guild.cs
+++ trunk/Server/Stump.Server.WorldServer/Game/Guilds/Guild.cs
@@ -576,9 +576,6 @@ namespace Stump.Server.WorldServer.Game.Guilds
                     (!kicker.GuildMember.HasRight(GuildRightsBitEnum.GUILD_RIGHT_BAN_MEMBERS) || kickedMember.IsBoss))
                     return false;
 
-                if (!KickMember(kickedMember, kickedMember.Id == kicker.GuildMember.Id))
-                    return false;
-
                 if (kicker.GuildMember.Id != kickedMember.Id)
                 {
                     // Vous avez banni <b>%1</b> de votre guilde.
@@ -586,7 +583,7 @@ namespace Stump.Server.WorldServer.Game.Guilds
                         kickedMember.Name);
                 }
 
-                return true;
+                return KickMember(kickedMember, kickedMember.Id == kicker.GuildMember.Id);
             }
         }
 
