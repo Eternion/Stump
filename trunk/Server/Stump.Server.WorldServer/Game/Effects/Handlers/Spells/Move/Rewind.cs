5a34b6557c7eacfc8b3dbfab9758b0f5a113cc38
Nami-Doc <vendethiel@hotmail.fr>
2014-03-10 20:46:16 +0100

    add missing files

---
+++ trunk/Server/Stump.Server.WorldServer/Game/Effects/Handlers/Spells/Move/Rewind.cs
@@ -0,0 +1,36 @@
+﻿using Stump.DofusProtocol.Enums;
+using Stump.Server.WorldServer.Database.World;
+using Stump.Server.WorldServer.Game.Actors.Fight;
+using Stump.Server.WorldServer.Game.Effects.Instances;
+using Stump.Server.WorldServer.Game.Fights.Buffs;
+using Stump.Server.WorldServer.Game.Spells;
+using Stump.Server.WorldServer.Handlers.Actions;
+using Stump.Server.WorldServer.Handlers.Context;
+
+namespace Stump.Server.WorldServer.Game.Effects.Handlers.Spells.Move
+{
+    [EffectHandler(EffectsEnum.Effect_Rewind)]
+    public class Rewind : SpellEffectHandler
+    {
+        public Rewind(EffectDice effect, FightActor caster, Spell spell, Cell targetedCell, bool critical)
+            : base(effect, caster, spell, targetedCell, critical)
+        {
+        }
+
+        public override bool Apply()
+        {
+            foreach (var actor in GetAffectedActors())
+            {
+                AddTriggerBuff(actor, true, BuffTriggerType.TURN_END, TriggerBuffApply);
+            }
+
+            return true;
+        }
+
+        public void TriggerBuffApply(TriggerBuff buff, BuffTriggerType trigger, object token)
+        {
+            buff.Target.Position.Cell = buff.Target.TurnStartPosition.Cell;
+            ActionsHandler.SendGameActionFightTeleportOnSameMapMessage(buff.Target.Fight.Clients, Caster, buff.Target, buff.Target.Position.Cell);
+        }
+    }
+}
---
+++ trunk/Server/Stump.Server.WorldServer/Game/Effects/Handlers/Spells/Move/Rollback.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using Stump.DofusProtocol.Enums;
+using Stump.Server.WorldServer.Database.World;
+using Stump.Server.WorldServer.Game.Actors.Fight;
+using Stump.Server.WorldServer.Game.Effects.Instances;
+using Stump.Server.WorldServer.Game.Fights.Buffs;
+using Stump.Server.WorldServer.Game.Spells;
+using Stump.Server.WorldServer.Handlers.Actions;
+using Stump.Server.WorldServer.Handlers.Context;
+
+namespace Stump.Server.WorldServer.Game.Effects.Handlers.Spells.Move
+{
+    [EffectHandler(EffectsEnum.Effect_Rollback)]
+    public class Rollback : SpellEffectHandler
+    {
+        public Rollback(EffectDice effect, FightActor caster, Spell spell, Cell targetedCell, bool critical)
+            : base(effect, caster, spell, targetedCell, critical)
+        {
+        }
+
+        public override bool Apply()
+        {
+            var fighters = Fight.GetAllFighters(f => f.IsAlive());
+            foreach (var fighter in fighters)
+            {
+                var newCell = fighter.FightStartPosition.Cell;
+                var oldFighter = Fight.GetOneFighter(newCell);
+                if (oldFighter != null)
+                    MoveOldFighter(oldFighter);
+                fighter.Position.Cell = newCell;
+
+                ActionsHandler.SendGameActionFightTeleportOnSameMapMessage(Fight.Clients, Caster, fighter, newCell);
+            }
+
+            return true;
+        }
+
+        private void MoveOldFighter(FightActor oldFighter)
+        {
+            var adjacentCell = oldFighter.Position.Point
+                .GetAdjacentCells(c => Fight.IsCellFree(Map.Cells[c]))
+                .FirstOrDefault();
+            if (adjacentCell != null)
+                oldFighter.Position.Cell = Map.Cells[adjacentCell.CellId];
+            else
+                oldFighter.Die();
+        }
+    }
+}
