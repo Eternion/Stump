17c6be66aa5610d7fa6cd9f0c8e8c3ae87bdf60c
Orochi <orochi@arkalys.com>
2014-04-01 10:42:04 +0200

    Add limit to TaxCollector ExperienceGathered

--- trunk/Server/Stump.Server.WorldServer/Game/Actors/RolePlay/TaxCollectors/TaxCollectorNpc.cs
+++ trunk/Server/Stump.Server.WorldServer/Game/Actors/RolePlay/TaxCollectors/TaxCollectorNpc.cs
@@ -36,6 +36,9 @@ namespace Stump.Server.WorldServer.Game.Actors.RolePlay.TaxCollectors
         [Variable]
         public static int BaseResistance = 25;
 
+        [Variable]
+        public int MaxGatheredXP = 1000000;
+
         public const int TAXCOLLECTOR_BONES = 714;
 
         private readonly WorldMapTaxCollectorRecord m_record;
@@ -92,6 +95,7 @@ namespace Stump.Server.WorldServer.Game.Actors.RolePlay.TaxCollectors
         }
 
         #region Properties
+
         public WorldMapTaxCollectorRecord Record
         {
             get
