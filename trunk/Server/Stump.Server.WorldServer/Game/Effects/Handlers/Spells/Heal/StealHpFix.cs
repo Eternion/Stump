50471530b448a3bd30ee964a9bdb1c2ba627437f
Nami-Doc <vendethiel@hotmail.fr>
2014-04-08 22:03:09 +0200

    steal hp fix ?

---
+++ trunk/Server/Stump.Server.WorldServer/Game/Effects/Handlers/Spells/Heal/StealHpFix.cs
@@ -0,0 +1,47 @@
+﻿using Stump.DofusProtocol.Enums;
+using Stump.Server.WorldServer.Database.World;
+using Stump.Server.WorldServer.Game.Actors.Fight;
+using Stump.Server.WorldServer.Game.Effects.Instances;
+using Stump.Server.WorldServer.Game.Fights.Buffs;
+using Spell = Stump.Server.WorldServer.Game.Spells.Spell;
+
+namespace Stump.Server.WorldServer.Game.Effects.Handlers.Spells.Heal
+{
+    [EffectHandler(EffectsEnum.Effect_StealHPFix)]
+    [EffectHandler(EffectsEnum.Effect_StealHPWater)]
+    [EffectHandler(EffectsEnum.Effect_StealHPEarth)]
+    [EffectHandler(EffectsEnum.Effect_StealHPAir)]
+    [EffectHandler(EffectsEnum.Effect_StealHPFire)]
+    [EffectHandler(EffectsEnum.Effect_StealHPNeutral)]
+    public class StealHpFix : SpellEffectHandler
+    {
+        public StealHpFix(EffectDice effect, FightActor caster, Spell spell, Cell targetedCell, bool critical)
+            : base(effect, caster, spell, targetedCell, critical)
+        {
+        }
+
+        public override bool Apply()
+        {
+            foreach (var actor in GetAffectedActors())
+            {
+                var integerEffect = GenerateEffect();
+
+                if (integerEffect == null)
+                    return false;
+
+                if (Effect.Duration > 0)
+                {
+                    AddTriggerBuff(actor, true, BuffTriggerType.TURN_BEGIN, OnBuffTriggered);
+                }
+                else
+                    ;
+            }
+
+            return true;
+        }
+
+        private void OnBuffTriggered(TriggerBuff buff, BuffTriggerType trigger, object token)
+        {
+        }
+    }
+}
