124e01645ee0d067fa9e115665a6e003a8993684
Nami-Doc <vendethiel@hotmail.fr>
2014-04-20 00:25:20 +0200

    improve stealhp... (still not quite there yet)

--- Server/Stump.Server.WorldServer/Game/Effects/Handlers/Spells/Heal/StealHpFix.cs
+++ Server/Stump.Server.WorldServer/Game/Effects/Handlers/Spells/Heal/StealHpFix.cs
@@ -1,4 +1,6 @@
-﻿using Stump.DofusProtocol.Enums;
+﻿using System;
+using System.Collections;
+using Stump.DofusProtocol.Enums;
 using Stump.Server.WorldServer.Database.World;
 using Stump.Server.WorldServer.Game.Actors.Fight;
 using Stump.Server.WorldServer.Game.Effects.Instances;
@@ -7,6 +9,11 @@ using Spell = Stump.Server.WorldServer.Game.Spells.Spell;
 
 namespace Stump.Server.WorldServer.Game.Effects.Handlers.Spells.Heal
 {
+    [EffectHandler(EffectsEnum.Effect_StealHPAir)]
+    [EffectHandler(EffectsEnum.Effect_StealHPEarth)]
+    [EffectHandler(EffectsEnum.Effect_StealHPFire)]
+    [EffectHandler(EffectsEnum.Effect_StealHPWater)]
+    [EffectHandler(EffectsEnum.Effect_StealHPNeutral)]
     [EffectHandler(EffectsEnum.Effect_StealHPFix)]
     public class StealHpFix : SpellEffectHandler
     {
@@ -28,6 +35,16 @@ namespace Stump.Server.WorldServer.Game.Effects.Handlers.Spells.Heal
                 {
                     AddTriggerBuff(actor, true, BuffTriggerType.TURN_BEGIN, OnBuffTriggered);
                 }
+                else
+                {
+                    var damage = new Fights.Damage(Dice, GetEffectSchool(Dice.EffectId), Caster, Spell);
+                    damage.GenerateDamages();
+
+                    if (Dice.EffectId == EffectsEnum.Effect_StealHPFix)
+                        actor.InflictDirectDamage(damage.Amount); // I could use a semicolon operator right here ...
+                    else
+                        actor.InflictDamage(damage);
+                }
             }
 
             return true;
@@ -35,6 +52,25 @@ namespace Stump.Server.WorldServer.Game.Effects.Handlers.Spells.Heal
 
         private static void OnBuffTriggered(TriggerBuff buff, BuffTriggerType trigger, object token)
         {
+        }
+
+        private EffectSchoolEnum GetEffectSchool(EffectsEnum effect)
+        {
+            switch (effect)
+            {
+                case EffectsEnum.Effect_StealHPAir:
+                    return EffectSchoolEnum.Air;
+                case EffectsEnum.Effect_StealHPEarth:
+                    return EffectSchoolEnum.Earth;
+                case EffectsEnum.Effect_StealHPFire:
+                    return EffectSchoolEnum.Fire;
+                case EffectsEnum.Effect_StealHPWater:
+                    return EffectSchoolEnum.Water;
+                case EffectsEnum.Effect_StealHPNeutral:
+                    return EffectSchoolEnum.Neutral;
+                default:
+                    throw new Exception(string.Format("Effect {0} has not associated School Type", effect));
+            }
         }
     }
 }
