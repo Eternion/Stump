727c3364b13ce6a41ccb43f052ea6ebe928eba5e
bouh2 <bouh22@hotmail.fr>
2014-05-03 20:14:00 +0200

    Have to try this yet

--- Server/Stump.Server.WorldServer/Game/Arena/ArenaFight.cs
+++ Server/Stump.Server.WorldServer/Game/Arena/ArenaFight.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Stump.DofusProtocol.Enums;
 using Stump.Server.WorldServer.Game.Actors.Fight;
+using Stump.Server.WorldServer.Game.Actors.RolePlay.Characters;
 using Stump.Server.WorldServer.Game.Fights;
 using Stump.Server.WorldServer.Game.Fights.Results;
 using Stump.Server.WorldServer.Game.Fights.Teams;
@@ -10,6 +12,14 @@ namespace Stump.Server.WorldServer.Game.Arena
 {
     public class ArenaFight : Fight<ArenaTeam, ArenaTeam>
     {
+        public event Action<ArenaFight, Character> FightDenied;
+
+        protected virtual void OnFightDenied(Character character)
+        {
+            Action<ArenaFight, Character> handler = FightDenied;
+            if (handler != null) handler(this, character);
+        }
+
         public ArenaFight(int id, Map fightMap, ArenaTeam blueTeam, ArenaTeam redTeam)
             : base(id, fightMap, blueTeam, redTeam)
         {
@@ -20,24 +30,22 @@ namespace Stump.Server.WorldServer.Game.Arena
             get { return FightTypeEnum.FIGHT_TYPE_PVP_ARENA; }
         }
 
+        public void DenyFight(Character character)
+        {
+            if (State != FightState.NotStarted)
+                throw new Exception("DenyFight() : State != FightState.NotStarted");
+
+            OnFightDenied(character);
+        }
+
         protected override IEnumerable<IFightResult> GenerateResults()
         {
-            throw new System.NotImplementedException();
-        }
-
-        protected override void SendGameFightJoinMessage(CharacterFighter fighter)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        protected override void SendGameFightJoinMessage(FightSpectator spectator)
-        {
-            throw new System.NotImplementedException();
+            yield break;
         }
 
         protected override bool CanCancelFight()
         {
-            throw new System.NotImplementedException();
+            return false;
         }
     }
 }
--- Server/Stump.Server.WorldServer/Handlers/Context/ContextArenaHandler.cs
+++ Server/Stump.Server.WorldServer/Handlers/Context/ContextArenaHandler.cs
@@ -1,15 +1,54 @@
 ﻿using System.Linq;
+using Stump.DofusProtocol.Enums;
 using Stump.DofusProtocol.Messages;
 using Stump.Server.BaseServer.Network;
+using Stump.Server.WorldServer.Core.Network;
 using Stump.Server.WorldServer.Game.Arena;
 
 namespace Stump.Server.WorldServer.Handlers.Context
 {
     public partial class ContextHandler
     {
+        [WorldHandler(GameRolePlayArenaFightAnswerMessage.Id)]
+        public static void HandleGameRolePlayArenaFightAnswerMessage(WorldClient client, GameRolePlayArenaFightAnswerMessage message)
+        {
+            var popup = client.Character.ArenaPopup;
+
+            if (popup == null)
+                return;
+
+            if (message.accept)
+                popup.Accept();
+            else
+                popup.Deny();
+        }
+
+        [WorldHandler(GameRolePlayArenaRegisterMessage.Id)]
+        public static void HandleGameRolePlayArenaRegisterMessage(WorldClient client, GameRolePlayArenaRegisterMessage message)
+        {
+            // 3VS3 only ?
+            ArenaManager.Instance.AddToQueue(client.Character);
+        }
+
+        [WorldHandler(GameRolePlayArenaUnregisterMessage.Id)]
+        public static void HandleGameRolePlayArenaUnregisterMessage(WorldClient client, GameRolePlayArenaUnregisterMessage message)
+        {
+            ArenaManager.Instance.RemoveFromQueue(client.Character);
+        }   
+        
         public static void SendGameRolePlayArenaFightPropositionMessage(IPacketReceiver client, ArenaPopup popup)
         {
-            client.Send(new GameRolePlayArenaFightPropositionMessage(popup.Member.Team.Fight.Id, popup.Member.Team.GetAlliesInQueue().Select(x => x.Id), 10));
+            client.Send(new GameRolePlayArenaFightPropositionMessage(popup.Team.Fight.Id, popup.Team.GetAlliesInQueue().Select(x => x.Id), 10));
+        }
+
+        public static void SendGameRolePlayArenaFighterStatusMessage(IPacketReceiver client)
+        {
+            client.Send(new GameRolePlayArenaFighterStatusMessage());
+        }
+
+        public static void SendGameRolePlayArenaRegistrationStatusMessage(IPacketReceiver client, bool registred, PvpArenaStepEnum step, PvpArenaTypeEnum type)
+        {
+            client.Send(new GameRolePlayArenaRegistrationStatusMessage(registred, (sbyte)step, (sbyte)type));
         }
     }
 }
