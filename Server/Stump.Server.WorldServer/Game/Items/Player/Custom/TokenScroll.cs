0e7f02402a2caa676878de086693cbb74bad372a
Orochi <orochi@arkalys.com>
2014-08-18 11:57:07 +0200

    Add TokenScroll item

---
+++ Server/Stump.Server.WorldServer/Game/Items/Player/Custom/TokenScroll.cs
@@ -0,0 +1,41 @@
+﻿using Stump.Core.Attributes;
+using Stump.Server.WorldServer.Database.Items;
+using Stump.Server.WorldServer.Database.World;
+using Stump.Server.WorldServer.Game.Actors.RolePlay.Characters;
+
+namespace Stump.Server.WorldServer.Game.Items.Player.Custom
+{
+    [ItemId(20715)]
+    public sealed class TokenScroll : BasePlayerItem
+    {
+        [Variable]
+        private const uint WonTokens = 100;
+
+        public TokenScroll(Character owner, PlayerItemRecord record)
+            : base(owner, record)
+        {
+        }
+
+        public override uint UseItem(int amount = 1, Cell targetCell = null, Character target = null)
+        {
+            Owner.Area.AddMessage(() =>
+            {
+                var tokens = Owner.Inventory.Tokens;
+
+                if (tokens != null)
+                {
+                    tokens.Stack += WonTokens;
+                    Owner.Inventory.RefreshItem(tokens); 
+                }
+                else
+                    Owner.Inventory.AddItem(Inventory.TokenTemplate, (int)WonTokens);
+
+
+                Owner.Inventory.Save();
+                Owner.SendServerMessage(string.Format("Vous avez reçu {0} Jetons en utilisant votre {1}", WonTokens, Template.Name));
+            });
+
+            return 1;
+        }
+    }
+}
