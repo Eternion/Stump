2ae56247bcf4fca3ebb86311863431f992ab0d19
bouh2 <bouh22@hotmail.fr>
2014-10-28 16:28:18 +0100

    Fix protocol builder (fixed array length)

--- Tools/DofusProtocolBuilder/XmlPatterns/XmlTypesBuilder.cs
+++ Tools/DofusProtocolBuilder/XmlPatterns/XmlTypesBuilder.cs
@@ -135,7 +135,7 @@ namespace DofusProtocolBuilder.XmlPatterns
                                                                     ( (AssignationStatement)entry ).Name == arrayMatch.Groups[1].Value
                                                                 let entryMatch =
                                                                     Regex.Match(( (AssignationStatement)entry ).Value,
-                                                                                @"new List<[\d\w\._]+>\(([\d]+)\)")
+                                                                                @"new List<[\d\w\._]+>\(([\d]+)")
                                                                 where entryMatch.Success
                                                                 select entryMatch.Groups[1].Value;
 
