79e58085714f2ee914287d844e3270b9c1fd7d77
Orochi <orochi@arkalys.com>
2015-02-21 01:03:00 +0100

    Find better way to respawn MonsterGroup after loosing fight

--- Server/Stump.Server.WorldServer/Game/Fights/FightPvM.cs
+++ Server/Stump.Server.WorldServer/Game/Fights/FightPvM.cs
@@ -52,12 +52,21 @@ namespace Stump.Server.WorldServer.Game.Fights
         {
             base.OnFightEnded();
 
-            if (Winners != DefendersTeam)
+            if (Winners == ChallengersTeam)
                 return;
 
             var monsterFighter = DefendersTeam.Leader;
-            if (monsterFighter != null)
-                Map.Enter(monsterFighter.Monster.Group);
+            if (monsterFighter == null)
+                return;
+
+            var spawningPool = monsterFighter.Monster.Group.SpawningPool;
+            if (spawningPool != null)
+            {
+                spawningPool.SetNextGroupToSpawn(monsterFighter.Monster.Group.GetMonsters());
+                spawningPool.SpawnNextGroup();
+            }
+            else
+                Map.SpawnMonsterGroup(monsterFighter.Monster.Group.GetMonsters().Select(x => x.Grade), Map.GetRandomFreePosition());
         }
 
         public override FightTypeEnum FightType
