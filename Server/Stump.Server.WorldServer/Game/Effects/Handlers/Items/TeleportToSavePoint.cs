2599add350225ec0fe3acbaee30107a16188f331
Orochi <orochi@arkalys.com>
2015-02-24 10:15:54 +0100

    Add TeleportToSavePoint

---
+++ Server/Stump.Server.WorldServer/Game/Effects/Handlers/Items/TeleportToSavePoint.cs
@@ -0,0 +1,25 @@
+﻿using Stump.DofusProtocol.Enums;
+using Stump.Server.WorldServer.Game.Actors.RolePlay.Characters;
+using Stump.Server.WorldServer.Game.Effects.Handlers.Usables;
+using Stump.Server.WorldServer.Game.Effects.Instances;
+using Stump.Server.WorldServer.Game.Items.Player;
+
+namespace Stump.Server.WorldServer.Game.Effects.Handlers.Items
+{
+    [EffectHandler(EffectsEnum.Effect_TeleportToSavePoint)]
+    public class TeleportToSavePoint : UsableEffectHandler
+    {
+        public TeleportToSavePoint(EffectBase effect, Character target, BasePlayerItem item)
+            : base(effect, target, item)
+        {
+        }
+
+        public override bool Apply()
+        {
+            var dest = Target.GetSpawnPoint();
+            Target.Teleport(dest);
+
+            return true;
+        }
+    }
+}
