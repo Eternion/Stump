80596193c06e7e0a79983d1c0e56577724b727b8
Orochi <orochi@arkalys.com>
2015-04-07 01:55:15 +0200

    Start Challenges

---
+++ Server/Stump.Server.WorldServer/Game/Fights/Challenges/Custom/DesigneVolontaireChallenge.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stump.DofusProtocol.Enums.Custom;
+using Stump.Server.WorldServer.Game.Actors.Fight;
+using Stump.Server.WorldServer.Handlers.Context;
+
+namespace Stump.Server.WorldServer.Game.Fights.Challenges.Custom
+{
+    [ChallengeIdentifier(3)]
+    public class DesigneVolontaireChallenge : DefaultChallenge
+    {
+        public DesigneVolontaireChallenge(int id, IFight fight)
+            : base(id, fight)
+        {
+            Bonus = 25;
+
+            Target = fight.GetRandomFighter<MonsterFighter>();
+
+            foreach (var fighter in Target.Team.Fighters)
+            {
+                fighter.Dead += OnDead;
+            }
+
+            TargetId = Target.Id;
+        }
+
+        private void OnDead(FightActor victim, FightActor killer)
+        {
+            if (victim == Target)
+                UpdateStatus(ChallengeStatusEnum.SUCCESS);
+            else if (victim != Target && Status == ChallengeStatusEnum.RUNNING)
+                UpdateStatus(ChallengeStatusEnum.FAILED);
+        }
+
+        public MonsterFighter Target
+        {
+            get;
+            private set;
+        }
+    }
+}
---
+++ Server/Stump.Server.WorldServer/Handlers/Context/ContextChallengeHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Stump.DofusProtocol.Enums.Custom;
+using Stump.DofusProtocol.Messages;
+using Stump.Server.BaseServer.Network;
+using Stump.Server.WorldServer.Game.Fights.Challenges;
+
+namespace Stump.Server.WorldServer.Handlers.Context
+{
+    public partial class ContextHandler
+    {
+        public static void SendChallengeInfoMessage(IPacketReceiver client, DefaultChallenge challenge)
+        {
+            client.Send(new ChallengeInfoMessage((short)challenge.Id, challenge.TargetId, challenge.Bonus, 0, challenge.Bonus, 0));
+        }
+
+        public static void SendChallengeResultMessage(IPacketReceiver client, DefaultChallenge challenge)
+        {
+            client.Send(new ChallengeResultMessage((short)challenge.Id, challenge.Status == ChallengeStatusEnum.SUCCESS));
+        }
+
+        public static void SendChallengeTargetsListMessage(IPacketReceiver client, IEnumerable<int> targetIds, IEnumerable<short> targetCells)
+        {
+            client.Send(new ChallengeTargetsListMessage(targetIds, targetCells));
+        }
+    }
+}
