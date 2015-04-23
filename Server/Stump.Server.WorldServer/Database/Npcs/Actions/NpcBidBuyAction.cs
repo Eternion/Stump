b5f588c7a11fddabe10cf0b8ab6733c9c8d0e4a2
Orochi <orochi@arkalys.com>
2015-04-23 09:41:56 +0200

    Start BidHouse

---
+++ Server/Stump.Server.WorldServer/Database/Npcs/Actions/NpcBidBuyAction.cs
@@ -0,0 +1,32 @@
+﻿using Stump.DofusProtocol.Enums;
+using Stump.DofusProtocol.Messages;
+using Stump.DofusProtocol.Types;
+using Stump.Server.BaseServer.Database;
+using Stump.Server.WorldServer.Game.Actors.RolePlay.Characters;
+using Stump.Server.WorldServer.Game.Actors.RolePlay.Npcs;
+
+namespace Stump.Server.WorldServer.Database.Npcs.Actions
+{
+    [Discriminator(Discriminator, typeof(NpcActionDatabase), typeof(NpcActionRecord))]
+    public class NpcBidBuyAction : NpcActionDatabase
+    {
+        public const string Discriminator = "Bid";
+
+        public NpcBidBuyAction(NpcActionRecord record)
+            : base(record)
+        {
+        }
+
+        public override NpcActionTypeEnum ActionType
+        {
+            get { return NpcActionTypeEnum.ACTION_BUY; }
+        }
+
+        public override void Execute(Npc npc, Character character)
+        {
+            character.Client.Send(new ExchangeStartedBidBuyerMessage(new SellerBuyerDescriptor(new[] {1, 10, 100}, new[] {2}, 2, 0, 63, npc.Id, 7)));
+
+            //RCV ExchangeBidHouseType -> ExchangeTypesExchangerDescriptionForUserMessage
+        }
+    }
+}
