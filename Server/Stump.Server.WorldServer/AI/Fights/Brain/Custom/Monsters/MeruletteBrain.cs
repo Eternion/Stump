158d9b5aa244f422438b3ad3c5fc1da8a576457a
Orochi <orochi@arkalys.com>
2015-05-17 16:58:05 +0200

    Fix some monsters spells Add Spell info command

---
+++ Server/Stump.Server.WorldServer/AI/Fights/Brain/Custom/Monsters/MeruletteBrain.cs
@@ -0,0 +1,23 @@
+﻿using Stump.DofusProtocol.Enums;
+using Stump.Server.WorldServer.Game.Actors.Fight;
+using Stump.Server.WorldServer.Game.Fights;
+using Stump.Server.WorldServer.Game.Spells;
+
+namespace Stump.Server.WorldServer.AI.Fights.Brain.Custom.Monsters
+{
+    [BrainIdentifier(2900)]
+    public class MeruletteBrain : Brain
+    {
+        public MeruletteBrain(AIFighter fighter)
+            : base(fighter)
+        {
+            fighter.Fight.TurnStarted += OnTurnStarted;
+        }
+
+        private static void OnTurnStarted(IFight fight, FightActor fighter)
+        {
+            fighter.CastSpell(new Spell((int)SpellIdEnum.MÉRULE_TRAÇON, 1), fighter.Cell, true, true);
+            fighter.Fight.TurnStarted -= OnTurnStarted;
+        }
+    }
+}
