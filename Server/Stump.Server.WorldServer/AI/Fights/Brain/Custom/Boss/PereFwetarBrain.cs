c4f4bf0c23825fb4a941c361150dc8376ac26e70
Orochi <orochi@arkalys.com>
2015-05-20 18:04:20 +0200

    Review PereFwetarBrain

--- Server/Stump.Server.WorldServer/AI/Fights/Brain/Custom/Boss/PereFwetarBrain.cs
+++ Server/Stump.Server.WorldServer/AI/Fights/Brain/Custom/Boss/PereFwetarBrain.cs
@@ -1,7 +1,11 @@
 ﻿using Stump.DofusProtocol.Enums;
+using Stump.Server.WorldServer.AI.Fights.Actions;
+using Stump.Server.WorldServer.AI.Fights.Spells;
 using Stump.Server.WorldServer.Game.Actors.Fight;
+using Stump.Server.WorldServer.Game.Effects.Handlers.Spells.Summon;
 using Stump.Server.WorldServer.Game.Fights;
 using Stump.Server.WorldServer.Game.Spells;
+using TreeSharp;
 
 namespace Stump.Server.WorldServer.AI.Fights.Brain.Custom.Boss
 {
@@ -21,6 +25,46 @@ namespace Stump.Server.WorldServer.AI.Fights.Brain.Custom.Boss
 
             fighter.CastSpell(new Spell((int)SpellIdEnum.VILAIN_GARNEMENT, 1), fighter.Cell, true, true);
             fighter.Fight.TurnStarted -= OnTurnStarted;
+        }
+
+        public override void Play()
+        {
+            var monster = Fighter as MonsterFighter;
+
+            if (monster == null)
+                return;
+           
+            var target = Environment.GetNearestFighter(x => x is SummonedMonster
+            && ((SummonedMonster) x).Monster.MonsterId == 494);
+
+            var spellId = (int) SpellIdEnum.PARADE_DES_VIEUX_JOUETS;
+
+            if (target == null)
+            {
+                target = Environment.GetNearestEnemy();
+                spellId = (int)SpellIdEnum.ASPIR_NENFAN;
+            }
+
+            var cell = Environment.GetCellToCastSpell(new TargetCell(target.Cell),
+                new Spell(spellId, (byte)monster.Monster.Grade.Level), true);
+
+            if (cell != null)
+            {
+                var action = new MoveAction(Fighter, cell);
+
+                foreach (var result in action.Execute(this))
+                {
+                    if (result == RunStatus.Failure)
+                        break;
+                }
+            }
+
+            if (spellId == (int)SpellIdEnum.PARADE_DES_VIEUX_JOUETS && target.Position.Point.IsAdjacentTo(monster.Position.Point))
+            {
+                Fighter.CastSpell(new Spell((int)SpellIdEnum.PARADE_DES_VIEUX_JOUETS, (byte)monster.Monster.Grade.Level), target.Cell);
+            }
+
+            base.Play();
         }
     }
 }
