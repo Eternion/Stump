5f04efbc70a46a61df9e3d2b482204b084997aca
Orochi <orochi@arkalys.com>
2015-05-20 11:48:48 +0200

    Add PereFwetarBrain Add NaughtyChildCastHandler Fix Parade des vieux jouets Fix Vilain Garnement

---
+++ Server/Stump.Server.WorldServer/AI/Fights/Brain/Custom/Boss/PereFwetarBrain.cs
@@ -0,0 +1,26 @@
+﻿using Stump.DofusProtocol.Enums;
+using Stump.Server.WorldServer.Game.Actors.Fight;
+using Stump.Server.WorldServer.Game.Fights;
+using Stump.Server.WorldServer.Game.Spells;
+
+namespace Stump.Server.WorldServer.AI.Fights.Brain.Custom.Boss
+{
+    [BrainIdentifier(1194)]
+    public class PereFwetarBrain : Brain
+    {
+        public PereFwetarBrain(AIFighter fighter)
+            : base(fighter)
+        {
+            fighter.Fight.TurnStarted += OnTurnStarted;
+        }
+
+        private void OnTurnStarted(IFight fight, FightActor fighter)
+        {
+            if (fighter != Fighter)
+                return;
+
+            fighter.CastSpell(new Spell((int)SpellIdEnum.VILAIN_GARNEMENT, 1), fighter.Cell, true, true);
+            fighter.Fight.TurnStarted -= OnTurnStarted;
+        }
+    }
+}
