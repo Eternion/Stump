b5cca7a0f1365996f68dbe5c50d9dc9489c4f001
Orochi <orochi@arkalys.com>
2015-05-29 06:56:27 +0200

    Few changes

--- DofusProtocol/D2oClasses/Tools/D2i/D2IFile.cs
+++ DofusProtocol/D2oClasses/Tools/D2i/D2IFile.cs
@@ -20,9 +20,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net;
-using Ionic.Zip;
-using Ionic.Zlib;
 using Stump.Core.IO;
 
 namespace Stump.DofusProtocol.D2oClasses.Tools.D2i
