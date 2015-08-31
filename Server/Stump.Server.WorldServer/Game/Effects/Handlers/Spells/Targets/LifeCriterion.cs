37e939e1313c6e01032119c85cd246d49930c348
bouh2 <bouh22@hotmail.fr>
2015-08-31 17:59:17 +0200

    Target criteria system -- DONT COMPILE

---
+++ Server/Stump.Server.WorldServer/Game/Effects/Handlers/Spells/Targets/LifeCriterion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stump.Server.WorldServer.Game.Actors.Fight;
+
+namespace Stump.Server.WorldServer.Game.Effects.Handlers.Spells.Targets
+{
+    public class LifeCriterion : TargetCriterion
+    {
+        public LifeCriterion(int lifePercent, bool mustBeGreater)
+        {
+            LifePercent = lifePercent;
+            MustBeGreater = mustBeGreater;
+        }
+
+        public int LifePercent
+        {
+            get;
+            set;
+        }
+
+        public bool MustBeGreater
+        {
+            get;
+            set;
+        }
+
+        public override bool IsTargetValid(FightActor actor, SpellEffectHandler handler)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
