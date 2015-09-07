b4023e1670b0a1310fe048ab04627aebb92cef75
Orochi <orochi@arkalys.com>
2015-09-07 10:18:51 +0200

    Add Effect_SummonTaxcollector Fix fight time in result frame

---
+++ Server/Stump.Server.WorldServer/Game/Effects/Handlers/Items/SummonTaxcollector.cs
@@ -0,0 +1,27 @@
+﻿using Stump.DofusProtocol.Enums;
+using Stump.Server.WorldServer.Game.Actors.RolePlay.Characters;
+using Stump.Server.WorldServer.Game.Actors.RolePlay.TaxCollectors;
+using Stump.Server.WorldServer.Game.Effects.Instances;
+using Stump.Server.WorldServer.Game.Items.Player;
+
+namespace Stump.Server.WorldServer.Game.Effects.Handlers.Items
+{
+    [EffectHandler(EffectsEnum.Effect_SummonTaxcollector)]
+    public class SummonTaxcollector : ItemEffectHandler
+    {
+        public SummonTaxcollector(EffectBase effect, Character target, BasePlayerItem item)
+            : base(effect, target, item)
+        {
+        }
+
+        public override bool Apply()
+        {
+            if (Target.Guild == null)
+                return false;
+
+            TaxCollectorManager.Instance.AddTaxCollectorSpawn(Target);
+
+            return true;
+        }
+    }
+}
