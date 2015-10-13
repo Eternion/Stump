d27be4e75dbda7f860f87d1c1876eb73dc577e3b
Orochi <orochi@arkalys.com>
2015-10-13 11:09:51 +0200

    Add SWL->SWF | SWF->SWL support in WorldEditor Add WebServerPlugin

---
+++ Plugins/Stump.Plugins.WebServerPlugin/HttpServer.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Stump.Plugins.WebServerPlugin
+{
+
+    public class HttpProcessor
+    {
+        public TcpClient socket;
+        public HttpServer srv;
+
+        Stream inputStream;
+        public StreamWriter outputStream;
+
+        public string http_method;
+        public string http_url;
+        public string http_protocol_versionstring;
+        public Hashtable httpHeaders = new Hashtable();
+
+
+        static int MAX_POST_SIZE = 10 * 1024 * 1024; // 10MB
+
+        public HttpProcessor(TcpClient s, HttpServer srv)
+        {
+            socket = s;
+            this.srv = srv;
+        }
+
+        static string streamReadLine(Stream inputStream)
+        {
+            int next_char;
+            var data = "";
+            while (true)
+            {
+                next_char = inputStream.ReadByte();
+                if (next_char == '\n') { break; }
+                if (next_char == '\r') { continue; }
+                if (next_char == -1) { Thread.Sleep(1); continue; };
+                data += Convert.ToChar(next_char);
+            }
+            return data;
+        }
+        public void process()
+        {
+            // we can't use a StreamReader for input, because it buffers up extra data on us inside it's
+            // "processed" view of the world, and we want the data raw after the headers
+            inputStream = new BufferedStream(socket.GetStream());
+
+            // we probably shouldn't be using a streamwriter for all output from handlers either
+            outputStream = new StreamWriter(new BufferedStream(socket.GetStream()));
+            try
+            {
+                parseRequest();
+                readHeaders();
+                if (http_method.Equals("GET"))
+                {
+                    handleGETRequest();
+                }
+                else if (http_method.Equals("POST"))
+                {
+                    handlePOSTRequest();
+                }
+            }
+            catch (Exception e)
+            {
+                writeFailure();
+            }
+
+            outputStream.Flush();
+            inputStream = null; outputStream = null;
+            socket.Close();
+        }
+
+        public void parseRequest()
+        {
+            var request = streamReadLine(inputStream);
+            var tokens = request.Split(' ');
+            if (tokens.Length != 3)
+                throw new Exception("invalid http request line");
+
+            http_method = tokens[0].ToUpper();
+            http_url = tokens[1];
+            http_protocol_versionstring = tokens[2];
+        }
+
+        public void readHeaders()
+        {
+            string line;
+            while ((line = streamReadLine(inputStream)) != null)
+            {
+                if (line.Equals(""))
+                    return;
+
+                var separator = line.IndexOf(':');
+                if (separator == -1)
+                {
+                    throw new Exception("invalid http header line: " + line);
+                }
+                var name = line.Substring(0, separator);
+                var pos = separator + 1;
+                while ((pos < line.Length) && (line[pos] == ' '))
+                {
+                    pos++; // strip any spaces
+                }
+
+                var value = line.Substring(pos, line.Length - pos);
+                httpHeaders[name] = value;
+            }
+        }
+
+        public void handleGETRequest()
+        {
+            srv.handleGETRequest(this);
+        }
+
+        const int BUF_SIZE = 4096;
+        public void handlePOSTRequest()
+        {
+            var content_len = 0;
+            var ms = new MemoryStream();
+            if (httpHeaders.ContainsKey("Content-Length"))
+            {
+                content_len = Convert.ToInt32(httpHeaders["Content-Length"]);
+                if (content_len > MAX_POST_SIZE)
+                {
+                    throw new Exception($"POST Content-Length({content_len}) too big for this simple server");
+                }
+                var buf = new byte[BUF_SIZE];
+                var to_read = content_len;
+                while (to_read > 0)
+                {
+                    var numread = inputStream.Read(buf, 0, Math.Min(BUF_SIZE, to_read));
+                    if (numread == 0)
+                    {
+                        if (to_read == 0)
+                        {
+                            break;
+                        }
+
+                        throw new Exception("client disconnected during post");
+                    }
+                    to_read -= numread;
+                    ms.Write(buf, 0, numread);
+                }
+                ms.Seek(0, SeekOrigin.Begin);
+            }
+            srv.handlePOSTRequest(this, new StreamReader(ms));
+
+        }
+
+        public void writeSuccess(string content_type = "text/html")
+        {
+            // this is the successful HTTP response line
+            outputStream.WriteLine("HTTP/1.0 200 OK");
+            // these are the HTTP headers...          
+            outputStream.WriteLine("Content-Type: " + content_type);
+            outputStream.WriteLine("Connection: close");
+            // ..add your own headers here if you like
+
+            outputStream.WriteLine(""); // this terminates the HTTP headers.. everything after this is HTTP body..
+        }
+
+        public void writeFailure()
+        {
+            // this is an http 404 failure response
+            outputStream.WriteLine("HTTP/1.0 404 File not found");
+            // these are the HTTP headers
+            outputStream.WriteLine("Connection: close");
+            // ..add your own headers here
+
+            outputStream.WriteLine(""); // this terminates the HTTP headers.
+        }
+    }
+
+    public abstract class HttpServer
+    {
+
+        protected int port;
+        TcpListener listener;
+        bool is_active = true;
+
+        protected HttpServer(int port)
+        {
+            this.port = port;
+        }
+
+        public void listen()
+        {
+            listener = new TcpListener(port);
+            listener.Start();
+            while (is_active)
+            {
+                var s = listener.AcceptTcpClient();
+                var processor = new HttpProcessor(s, this);
+                var thread = new Thread(new ThreadStart(processor.process));
+                thread.Start();
+                Thread.Sleep(1);
+            }
+        }
+
+        public abstract void handleGETRequest(HttpProcessor p);
+        public abstract void handlePOSTRequest(HttpProcessor p, StreamReader inputData);
+    }
+}
