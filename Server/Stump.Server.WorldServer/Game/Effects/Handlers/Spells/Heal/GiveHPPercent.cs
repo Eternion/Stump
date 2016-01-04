eb7a27b28c1d3bbefcd5186c37e1ac4bdbdb7f23
Orochi <orochi@arkalys.com>
2016-01-04 02:07:45 +0100

    Fix GiveHPPercent

--- Server/Stump.Server.WorldServer/Game/Effects/Handlers/Spells/Heal/GiveHPPercent.cs
+++ Server/Stump.Server.WorldServer/Game/Effects/Handlers/Spells/Heal/GiveHPPercent.cs
@@ -21,30 +21,29 @@ namespace Stump.Server.WorldServer.Game.Effects.Handlers.Spells.Heal
             if (integerEffect == null)
                 return false;
 
+            var damageAmount = 0;
             foreach (var actor in GetAffectedActors())
             {
                 HealHpPercent(actor, integerEffect.Value);
 
                 if (Effect.Duration == 0)
-                    DealHpPercent(integerEffect.Value);
+                    damageAmount = DealHpPercent(integerEffect.Value);
             }
+
+            if (damageAmount > 0)
+                Caster.InflictDirectDamage(damageAmount, Caster);
 
             return true;
         }
 
         // Todo: reduce duplication (see RestoreHpPercent)
-        private void HealHpPercent(FightActor actor, int percent)
+        void HealHpPercent(FightActor actor, int percent)
         {
             var healAmount = (int)(Caster.LifePoints * (percent / 100d));
 
             actor.Heal(healAmount, Caster, false);
         }
 
-        private void DealHpPercent(int percent)
-        {
-            var damageAmount = (int)(Caster.LifePoints * (percent / 100d));
-
-            Caster.InflictDirectDamage(damageAmount, Caster);
-        }
+        int DealHpPercent(int percent) => (int)(Caster.LifePoints * (percent / 100.0));
     }
 }
