3bfb52edc9e7c52d382679579159bddf4b7c8d39
Orochi <orochi@arkalys.com>
2016-01-05 19:17:56 +0100

    Fix a bug with critical command

--- Server/Stump.Server.WorldServer/Game/Actors/Fight/FightActor.cs
+++ Server/Stump.Server.WorldServer/Game/Actors/Fight/FightActor.cs
@@ -749,7 +749,8 @@ namespace Stump.Server.WorldServer.Game.Actors.Fight
 
             Fight.StartSequence(SequenceTypeEnum.SEQUENCE_SPELL);
 
-            var critical = (bool)(this as CharacterFighter)?.Character.CriticalMode ? FightSpellCastCriticalEnum.CRITICAL_HIT : RollCriticalDice(spellLevel);
+            var character = (this as CharacterFighter);
+            var critical = (character != null && character.Character.CriticalMode) ? FightSpellCastCriticalEnum.CRITICAL_HIT : RollCriticalDice(spellLevel);
 
             if (critical == FightSpellCastCriticalEnum.CRITICAL_FAIL)
             {
