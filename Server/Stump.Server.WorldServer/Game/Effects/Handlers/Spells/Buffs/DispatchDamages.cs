9739155a79af00f536e032ac8945ce07d7051be0
Orochi <orochi@arkalys.com>
2016-01-25 16:02:52 +0100

    Fix DispatchDamages effect

--- Server/Stump.Server.WorldServer/Game/Effects/Handlers/Spells/Buffs/DispatchDamages.cs
+++ Server/Stump.Server.WorldServer/Game/Effects/Handlers/Spells/Buffs/DispatchDamages.cs
@@ -8,10 +8,10 @@ using Spell = Stump.Server.WorldServer.Game.Spells.Spell;
 
 namespace Stump.Server.WorldServer.Game.Effects.Handlers.Spells.Buffs
 {
-    [EffectHandler(EffectsEnum.Effect_Putsch)]
-    public class Putsch : SpellEffectHandler
+    [EffectHandler(EffectsEnum.Effect_DispatchDamages)]
+    public class DispatchDamages : SpellEffectHandler
     {
-        public Putsch(EffectDice effect, FightActor caster, Spell spell, Cell targetedCell, bool critical)
+        public DispatchDamages(EffectDice effect, FightActor caster, Spell spell, Cell targetedCell, bool critical)
             : base(effect, caster, spell, targetedCell, critical)
         {
         }
@@ -20,16 +20,13 @@ namespace Stump.Server.WorldServer.Game.Effects.Handlers.Spells.Buffs
         {
             foreach (var actor in GetAffectedActors())
             {
-                var buffId = actor.PopNextBuffId();
-                var buff = new TriggerBuff(buffId, actor, Caster, Dice, Spell, Spell, false, false, PutschBuffTrigger);
-                buff.SetTrigger(BuffTriggerType.AfterDamaged);
-                actor.AddBuff(buff);
+                AddTriggerBuff(actor, false, BuffTrigger);
             }
 
             return true;
         }
 
-        private void PutschBuffTrigger(TriggerBuff buff, FightActor triggerrer, BuffTriggerType trigger, object token)
+        void BuffTrigger(TriggerBuff buff, FightActor triggerrer, BuffTriggerType trigger, object token)
         {
             var damage = token as Fights.Damage;
             if (damage == null)
@@ -45,12 +42,12 @@ namespace Stump.Server.WorldServer.Game.Effects.Handlers.Spells.Buffs
                 return;
 
             var target = buff.Target;
-            var damages = (int)(damage.Amount * (Dice.DiceNum / 100d));
+            var damages = (int)(damage.Amount * (Dice.DiceNum / 100.0));
             var cells = target.Position.Point.GetAdjacentCells(x => !Fight.IsCellFree(target.Map.Cells[x]));
 
             foreach (var actor in cells.Select(cell => target.Fight.GetOneFighter(target.Map.Cells[cell.CellId])).Where(actor => actor != null))
             {
-                var putschDamage = new Fights.Damage(damages)
+                var newDamages = new Fights.Damage(damages)
                 {
                     Source = target,
                     School = damage.School,
@@ -60,10 +57,10 @@ namespace Stump.Server.WorldServer.Game.Effects.Handlers.Spells.Buffs
                     Spell = null
                 };
 
-                actor.InflictDamage(putschDamage);
+                actor.InflictDamage(newDamages);
             }
 
-            var putschTargetDamage = new Fights.Damage(damages)
+            var newTargetDamages = new Fights.Damage(damages)
             {
                 Source = damage.Source,
                 School = damage.School,
@@ -73,7 +70,7 @@ namespace Stump.Server.WorldServer.Game.Effects.Handlers.Spells.Buffs
                 Spell = null
             };
 
-            target.InflictDamage(putschTargetDamage);
+            target.InflictDamage(newTargetDamages);
         }
     }
 }
