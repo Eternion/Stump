2a9727ed3062b18216576557a924c298e8819ee8
Orochi <orochi@arkalys.com>
2016-01-25 12:02:22 +0100

    Mistake

--- Server/Stump.Server.WorldServer/Handlers/Basic/BasicHandler.cs
+++ Server/Stump.Server.WorldServer/Handlers/Basic/BasicHandler.cs
@@ -95,7 +95,7 @@ namespace Stump.Server.WorldServer.Handlers.Basic
         public static void SendBasicTimeMessage(IPacketReceiver client)
         {
             var offset = (short) TimeZone.CurrentTimeZone.GetUtcOffset(DateTime.Now).TotalMinutes;
-            client.Send(new BasicTimeMessage(DateTime.Now.GetUnixTimeStampDouble(), offset));
+            client.Send(new BasicTimeMessage(DateTime.Now.GetUnixTimeStampLong(), offset));
         }
 
         public static void SendBasicNoOperationMessage(IPacketReceiver client)
