8d0a35163c738eb995e13e75537bd82dbb28df51
Orochi <orochi@arkalys.com>
2016-01-27 23:43:00 +0100

    Fix TemporalDust

---
+++ Server/Stump.Server.WorldServer/Game/Spells/Casts/Xelor/TemporalDustCastHandler.cs
@@ -0,0 +1,23 @@
+﻿using Stump.DofusProtocol.Enums;
+using Stump.Server.WorldServer.Database.World;
+using Stump.Server.WorldServer.Game.Actors.Fight;
+
+namespace Stump.Server.WorldServer.Game.Spells.Casts.Xelor
+{
+    [SpellCastHandler(SpellIdEnum.POUSSIÈRE_TEMPORELLE)]
+    public class TemporalDustCastHandler : DefaultSpellCastHandler
+    {
+        public TemporalDustCastHandler(FightActor caster, Spell spell, Cell targetedCell, bool critical)
+            : base(caster, spell, targetedCell, critical)
+        {
+        }
+
+        public override void Execute()
+        {
+            Handlers[5].SetAffectedActors(Handlers[5].GetAffectedActors(x => !x.NeedTelefragState));
+            Handlers[6].SetAffectedActors(Handlers[5].GetAffectedActors(x => !x.NeedTelefragState));
+
+            base.Execute();
+        }
+    }
+}
