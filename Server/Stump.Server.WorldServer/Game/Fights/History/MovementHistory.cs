1e4dff4aa5569b9a346c2dcfd2ed09d96807ddb9
timorem <timorem@hotmail.fr>
2016-02-16 14:21:05 +0100

    Gelure fix

--- Server/Stump.Server.WorldServer/Game/Fights/History/MovementHistory.cs
+++ Server/Stump.Server.WorldServer/Game/Fights/History/MovementHistory.cs
@@ -71,7 +71,7 @@ namespace Stump.Server.WorldServer.Game.Fights.History
         {
             var previous = GetPreviousPosition();
 
-            return CurrentRound - previous?.Round < lifetime ? previous : null;
+            return CurrentRound - previous?.Round <= lifetime ? previous : null;
         }
 
         public MovementHistoryEntry PopPreviousPosition(int lifetime)
@@ -88,7 +88,7 @@ namespace Stump.Server.WorldServer.Game.Fights.History
         {
             var entry = PopPreviousPosition(lifetime);
             
-            while (entry != null && predicate(entry) && CurrentRound - entry.Round < lifetime)
+            while (entry != null && predicate(entry) && CurrentRound - entry.Round <= lifetime)
                 entry = PopPreviousPosition();
 
             return entry;
@@ -98,6 +98,6 @@ namespace Stump.Server.WorldServer.Game.Fights.History
 
         public IEnumerable<MovementHistoryEntry> GetEntries(Predicate<MovementHistoryEntry> predicate) => m_underlyingStack.Where(entry => predicate(entry));
 
-        public IEnumerable<MovementHistoryEntry> GetEntries(int lifetime) => GetEntries(x => CurrentRound - x.Round < lifetime);
+        public IEnumerable<MovementHistoryEntry> GetEntries(int lifetime) => GetEntries(x => CurrentRound - x.Round <= lifetime);
     }
 }
