96a9ffcc11843b13a6019d05290388caa9b0cc59
Orochi <orochi@arkalys.com>
2016-03-28 21:03:48 +0200

    Fix Coup de Poing Fix IncreaseSize Fix a possible glitch on kamas exchanges Add AlignmentReply

---
+++ Server/Stump.Server.WorldServer/Database/Npcs/Replies/AlignmentReply.cs
@@ -0,0 +1,41 @@
+﻿using Stump.DofusProtocol.Enums;
+using Stump.Server.BaseServer.Database;
+using Stump.Server.WorldServer.Game.Actors.RolePlay.Characters;
+using Stump.Server.WorldServer.Game.Actors.RolePlay.Npcs;
+
+namespace Stump.Server.WorldServer.Database.Npcs.Replies
+{
+    [Discriminator("Alignment", typeof(AlignmentReply), typeof(NpcReplyRecord))]
+    public class AlignmentReply : NpcReply
+    {
+        public AlignmentReply(NpcReplyRecord record)
+            : base(record)
+        {
+        }
+
+        /// <summary>
+        /// Parameter 0
+        /// </summary>
+        public int Side
+        {
+            get
+            {
+                return Record.GetParameter<int>(0);
+            }
+            set
+            {
+                Record.SetParameter(0, value);
+            }
+        }
+
+        public override bool Execute(Npc npc, Character character)
+        {
+            if (!base.Execute(npc, character))
+                return false;
+
+            character.ChangeAlignementSide((AlignmentSideEnum)Side);
+
+            return true;
+        }
+    }
+}
