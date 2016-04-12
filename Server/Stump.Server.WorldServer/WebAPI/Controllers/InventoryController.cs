97967a3ee042512d2fae111c3d32c52d08e031e9
Orochi <orochi@arkalys.com>
2016-04-12 12:18:41 +0200

    Work on InventoryController

--- Server/Stump.Server.WorldServer/WebAPI/Controllers/InventoryController.cs
+++ Server/Stump.Server.WorldServer/WebAPI/Controllers/InventoryController.cs
@@ -1,5 +1,7 @@
 ﻿using Stump.Server.WorldServer.Game;
+using Stump.Server.WorldServer.Game.Items;
 using System.Linq;
+using System.Net;
 using System.Web.Http;
 
 namespace Stump.Server.WorldServer.WebAPI.Controllers
@@ -12,7 +14,7 @@ namespace Stump.Server.WorldServer.WebAPI.Controllers
             var character = World.Instance.GetCharacter(characterId);
 
             if (character == null)
-                return NotFound();
+                return StatusCode(HttpStatusCode.BadRequest);
 
             return Json(character.Inventory.GetItems().Select(x => x.GetObjectItem()));
         }
@@ -20,22 +22,58 @@ namespace Stump.Server.WorldServer.WebAPI.Controllers
         [Route("Character/{characterId:int}/Inventory/{guid:int}")]
         public IHttpActionResult Get(int characterId, int guid)
         {
-            return Ok($"value: {guid}");
+            var character = World.Instance.GetCharacter(characterId);
+
+            if (character == null)
+                return StatusCode(HttpStatusCode.BadRequest);
+
+            var item = character.Inventory.TryGetItem(guid);
+
+            if (item == null)
+                return StatusCode(HttpStatusCode.BadRequest);
+
+            return Json(item.GetObjectItem());
         }
 
-        public IHttpActionResult Post(int characterId, string value)
+        public IHttpActionResult Post(int characterId, string value) => StatusCode(HttpStatusCode.MethodNotAllowed);
+
+        [Route("Character/{characterId:int}/Inventory/{itemId:int}/{amount:int}")]
+        public IHttpActionResult Put(int characterId, int itemId, int amount)
         {
-            return Unauthorized();
+            var character = World.Instance.GetCharacter(characterId);
+
+            if (character == null)
+                return StatusCode(HttpStatusCode.BadRequest);
+
+            var item = ItemManager.Instance.CreatePlayerItem(character, itemId, amount);
+
+            if (item == null)
+                return StatusCode(HttpStatusCode.BadRequest);
+
+            var playerItem = character.Inventory.AddItem(item);
+
+            if (playerItem == null)
+                return StatusCode(HttpStatusCode.BadRequest);
+
+            return Ok();
         }
 
-        public IHttpActionResult Put(int characterId, int guid, string value)
+        [Route("Character/{characterId:int}/Inventory/{guid:int}/{amount:int}")]
+        public IHttpActionResult Delete(int characterId, int guid, int amount)
         {
-            return Unauthorized();
-        }
+            var character = World.Instance.GetCharacter(characterId);
 
-        public IHttpActionResult Delete(int characterId, int guid)
-        {
-            return Unauthorized();
+            if (character == null)
+                return StatusCode(HttpStatusCode.BadRequest);
+
+            var item = character.Inventory.TryGetItem(guid);
+
+            if (item == null)
+                return StatusCode(HttpStatusCode.BadRequest);
+
+            character.Inventory.UnStackItem(item, amount);
+
+            return Ok();
         }
     }
 }
