6eb77ddb9b2c943bf9cdd1b2a4cc476ebc1eaf74
Orochi <orochi@arkalys.com>
2016-04-23 13:13:38 +0200

    Add Effect_HealBuff Add LapinoBrain Fix glyphs triggerType

--- Server/Stump.Server.WorldServer/Game/Fights/Triggers/Glyph.cs
+++ Server/Stump.Server.WorldServer/Game/Fights/Triggers/Glyph.cs
@@ -1,11 +1,9 @@
 using System.Drawing;
-using System.Linq;
 using Stump.DofusProtocol.Enums;
 using Stump.DofusProtocol.Types;
 using Stump.Server.WorldServer.Database.World;
 using Stump.Server.WorldServer.Game.Actors.Fight;
 using Stump.Server.WorldServer.Game.Effects.Instances;
-using Stump.Server.WorldServer.Game.Items.Player.Custom.LivingObjects;
 using Stump.Server.WorldServer.Game.Spells;
 using Spell = Stump.Server.WorldServer.Game.Spells.Spell;
 
@@ -13,21 +11,15 @@ namespace Stump.Server.WorldServer.Game.Fights.Triggers
 {
     public class Glyph : MarkTrigger
     {
-        static readonly int[] SPELLS_GLYPH_END_TURN =
-        {
-            (int)SpellIdEnum.GLYPHE_DE_RÉPULSION,
-            (int)SpellIdEnum.GLYPHE_DE_RÉPULSION_DU_DOPEUL,
-            (int)SpellIdEnum.GLYPHE_DAIVAIN
-        };
-
         public Glyph(short id, FightActor caster, Spell castedSpell, EffectDice originEffect, Spell glyphSpell,
-                     Cell centerCell, SpellShapeEnum shape, byte minSize, byte size, Color color, bool canBeForced)
+                     Cell centerCell, SpellShapeEnum shape, byte minSize, byte size, Color color, bool canBeForced, TriggerType triggerType = TriggerType.OnTurnBegin)
             : base(id, caster, castedSpell, originEffect, centerCell,
                 new MarkShape(caster.Fight, centerCell, shape, GetMarkShape(shape), minSize, size, color))
         {
             GlyphSpell = glyphSpell;
             CanBeForced = canBeForced;
             Duration = originEffect.Duration;
+            TriggerType = triggerType;
         }
 
         public Spell GlyphSpell
@@ -48,7 +40,10 @@ namespace Stump.Server.WorldServer.Game.Fights.Triggers
 
         public override GameActionMarkTypeEnum Type => GameActionMarkTypeEnum.GLYPH;
 
-        public override TriggerType TriggerType => (SPELLS_GLYPH_END_TURN.Contains(CastedSpell.Id) ? TriggerType.OnTurnEnd : TriggerType.OnTurnBegin);
+        public override TriggerType TriggerType
+        {
+            get;
+        }
 
         public override bool DecrementDuration()
         {
@@ -85,26 +80,14 @@ namespace Stump.Server.WorldServer.Game.Fights.Triggers
             handler.Execute();
         }
 
-
         public override GameActionMark GetGameActionMark()
-        {
-            return new GameActionMark(Caster.Id, (sbyte)Caster.Team.Id, CastedSpell.Template.Id, (sbyte)CastedSpell.CurrentLevel, Id, (sbyte)Type, CenterCell.Id,
+            => new GameActionMark(Caster.Id, (sbyte)Caster.Team.Id, CastedSpell.Template.Id, (sbyte)CastedSpell.CurrentLevel, Id, (sbyte)Type, CenterCell.Id,
                                       Shape.GetGameActionMarkedCells(), true);
-        }
 
-        public override GameActionMark GetHiddenGameActionMark()
-        {
-            return GetGameActionMark();
-        }
+        public override GameActionMark GetHiddenGameActionMark() => GetGameActionMark();
 
-        public override bool DoesSeeTrigger(FightActor fighter)
-        {
-            return true;
-        }
+        public override bool DoesSeeTrigger(FightActor fighter) => true;
 
-        public override bool CanTrigger(FightActor actor)
-        {
-            return true;
-        }
+        public override bool CanTrigger(FightActor actor) => true;
     }
 }
