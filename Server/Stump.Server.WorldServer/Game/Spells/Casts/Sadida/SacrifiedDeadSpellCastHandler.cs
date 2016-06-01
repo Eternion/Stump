da5bae7282afdd79e3495ef16bbb1cf5fa3df386
timorem <timorem@hotmail.fr>
2016-06-01 19:29:54 +0200

    Fix bug sadidas

---
+++ Server/Stump.Server.WorldServer/Game/Spells/Casts/Sadida/SacrifiedDeadSpellCastHandler.cs
@@ -0,0 +1,27 @@
+﻿using Stump.Server.WorldServer.Database.World;
+using Stump.Server.WorldServer.Game.Actors.Fight;
+
+namespace Stump.Server.WorldServer.Game.Spells.Casts.Sadida
+{
+    [SpellCastHandler(6718)]
+    public class SacrifiedDeadSpellCastHandler : DefaultSpellCastHandler
+    {
+        public SacrifiedDeadSpellCastHandler(FightActor caster, Spell spell, Cell targetedCell, bool critical)
+            : base(caster, spell, targetedCell, critical)
+        {
+            
+        }
+
+        public override bool Initialize()
+        {
+            if (base.Initialize() && CastedByEffect != null)
+            {
+                Handlers[0].SetAffectedActors(CastedByEffect.GetAffectedActors());
+                return true;
+            }
+
+            return false;
+        }
+        
+    }
+}
