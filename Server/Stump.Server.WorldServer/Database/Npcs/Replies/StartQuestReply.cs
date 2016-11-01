1c8a59c03049fd46ac8bffd58c06dc2a744c168c
timorem <timorem@hotmail.fr>
2016-11-01 18:24:53 +0100

    Fix npc dialog

--- Server/Stump.Server.WorldServer/Database/Npcs/Replies/StartQuestReply.cs
+++ Server/Stump.Server.WorldServer/Database/Npcs/Replies/StartQuestReply.cs
@@ -5,6 +5,9 @@ namespace Stump.Server.WorldServer.Database.Npcs.Replies
     [Discriminator("Quest", typeof (NpcReply), typeof (NpcReplyRecord))]
     public class StartQuestReply
     {
-         
+        public StartQuestReply(NpcReplyRecord record)
+        {
+            
+        }
     }
 }
