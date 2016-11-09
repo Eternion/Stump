703cae577f45e72b7714a01d65df2ce44a79fcfd
timorem <timorem@hotmail.fr>
2016-11-09 15:27:47 +0100

    fix interactives spawn

---
+++ Tools/DBSynchroniser/Interactives/IdentifiableElement.cs
@@ -0,0 +1,31 @@
+﻿using Stump.DofusProtocol.D2oClasses.Tools.Dlm;
+
+namespace DBSynchroniser.Interactives
+{
+    public class IdentifiableElement
+    {
+        public IdentifiableElement(DlmGraphicalElement element, DlmMap map)
+        {
+            Element = element;
+            Map = map;
+        }
+
+        public DlmGraphicalElement Element
+        {
+            get;
+            set;
+        }
+        
+        public DlmMap Map
+        {
+            get;
+            set;
+        }
+
+        public bool Ignore
+        {
+            get;
+            set;
+        }
+    }
+}
