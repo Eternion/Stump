f6cb9fe3afa6f90e429633fe190edee5e5e55b5e
timorem <timorem@hotmail.fr>
2016-11-17 11:01:17 +0100

    Fix alternative dungeons

--- Server/Stump.Server.WorldServer/Game/Actors/RolePlay/Monsters/MonsterGroupWithAlternatives.cs
+++ Server/Stump.Server.WorldServer/Game/Actors/RolePlay/Monsters/MonsterGroupWithAlternatives.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Stump.DofusProtocol.Types;
 using Stump.Server.WorldServer.Game.Actors.Fight;
 using Stump.Server.WorldServer.Game.Actors.RolePlay.Characters;
+using Stump.Server.WorldServer.Game.Fights;
 using Stump.Server.WorldServer.Game.Fights.Teams;
 using Stump.Server.WorldServer.Game.Maps.Cells;
 using Stump.Server.WorldServer.Game.Maps.Spawns;
@@ -11,6 +13,7 @@ namespace Stump.Server.WorldServer.Game.Actors.RolePlay.Monsters
 {
     public class MonsterGroupWithAlternatives : MonsterGroup
     {
+        private int m_lastGroupMinPlayers ;
         private Dictionary<int, List<Monster>> m_monstersByMembersCount = new Dictionary<int, List<Monster>>();
 
         public MonsterGroupWithAlternatives(int id, ObjectPosition position, SpawningPoolBase spawningPool = null)
@@ -27,13 +30,41 @@ namespace Stump.Server.WorldServer.Game.Actors.RolePlay.Monsters
             base.AddMonster(monster);
         }
 
+        protected override int CountInitialFighters() => m_monstersByMembersCount.OrderByDescending(x => x.Key).FirstOrDefault(x => x.Key <= 1).Value.Count;
+
         public override IEnumerable<MonsterFighter> CreateFighters(FightMonsterTeam team)
         {
-            var membersCount = (team.OpposedTeam.Leader as CharacterFighter)?.Character.Party?.MembersCount ?? 1;
+            var group = m_monstersByMembersCount.OrderByDescending(x => x.Key).FirstOrDefault(x => x.Key <= 1).Value ?? GetMonsters();
 
-            var group = m_monstersByMembersCount.OrderBy(x => x.Key).FirstOrDefault(x => x.Key <= membersCount).Value ?? GetMonsters();
+            team.OpposedTeam.FighterAdded += OnFighterAddedOrRemoved;
+            team.OpposedTeam.FighterRemoved += OnFighterAddedOrRemoved;
+            team.Fight.FightStarted += FightOnFightStarted;
+            m_lastGroupMinPlayers = 1;
 
             return group.Select(x => x.CreateFighter(team));
+        }
+
+        private void FightOnFightStarted(IFight fight)
+        {
+            fight.ChallengersTeam.FighterAdded -= OnFighterAddedOrRemoved;
+            fight.ChallengersTeam.FighterRemoved -= OnFighterAddedOrRemoved;
+        }
+
+        private void OnFighterAddedOrRemoved(FightTeam team, FightActor fighter)
+        {
+            var key = m_monstersByMembersCount.OrderByDescending(x => x.Key).FirstOrDefault(x => x.Key <= team.Fighters.Count).Key;
+
+            if (key > 0 && key != m_lastGroupMinPlayers)
+            {
+                var group = m_monstersByMembersCount[key];
+
+                team.Fight.DefendersTeam.RemoveAllFighters();
+
+                foreach (var monster in group)
+                    team.Fight.DefendersTeam.AddFighter(monster.CreateFighter(team.Fight.DefendersTeam));
+
+                m_lastGroupMinPlayers = key;
+            }
         }
 
         public override GroupMonsterStaticInformations GetGroupMonsterStaticInformations(Character character)
