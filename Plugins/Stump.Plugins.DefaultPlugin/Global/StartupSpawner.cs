f42a38ebe7320a62a37e85372121f083256bc0b7
timorem <timorem@hotmail.fr>
2017-02-09 17:29:11 +0100

    Add startup spawner in default plugin

---
+++ Plugins/Stump.Plugins.DefaultPlugin/Global/StartupSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using Stump.Server.BaseServer.Initialization;
+using Stump.Server.WorldServer.Game;
+using Stump.Server.WorldServer.Game.Maps.Spawns;
+
+namespace Stump.Plugins.DefaultPlugin.Global
+{
+    public static class StartupSpawner
+    {
+        [Initialization(typeof(World), Silent=true)]
+        public static void SpawnAllMaps()
+        {
+            foreach (var map in World.Instance.GetMaps())
+            {
+                foreach(var pool in map.SpawningPools.OfType<ClassicalSpawningPool>())
+                {
+                    while (pool.SpawnNextGroup())
+                        ;
+                }
+            }
+        }
+    }
+}
